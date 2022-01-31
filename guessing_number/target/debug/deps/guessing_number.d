3532ee9885c077538e364a604708d4f993065fbc
muclover <guitang001@gmail.com>
2022-01-31 22:09:18 +0800

    second commit

---
+++ guessing_number/target/debug/deps/guessing_number.d
@@ -0,0 +1,5 @@
+E:\rust_learning_code\Rust_Learning_Code\guessing_number\target\debug\deps\guessing_number.exe: src\main.rs
+
+E:\rust_learning_code\Rust_Learning_Code\guessing_number\target\debug\deps\guessing_number.d: src\main.rs
+
+src\main.rs:
---
+++ guessing_number/target/debug/guessing_number.d
@@ -0,0 +1,1 @@
+E:\rust_learning_code\Rust_Learning_Code\guessing_number\target\debug\guessing_number.exe: E:\rust_learning_code\Rust_Learning_Code\guessing_number\src\main.rs
